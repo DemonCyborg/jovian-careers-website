a54917503cf20e2dea21e392d2f6ee3af4482c63
moddevs <moderndevelopers01@gmail.com>
2023-09-02 12:13:29 +0530

    initail commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hellow World"
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=True)
